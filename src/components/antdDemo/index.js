ddbf93fe98b49ec7cc02200572b734102c595f50
17600596130 <A932635@163.com>
2018-09-20 19:05:07 +0800

    feat( antd Demo )

--- src/components/antdDemo/index.js
+++ src/components/antdDemo/index.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
+import { Layout, Button, Dropdown, Icon, Menu } from 'antd';
+import './index.scss';
 
+const { Header, Footer, Content } = Layout;
 
 class AntdDemo extends Component {
   render() {
+    const menu = (
+      <Menu>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
+        </Menu.Item>
+      </Menu>
+    );
+
     return (
-      <div>antdDemo</div>
+      <Layout className="antd-demo">
+        <Header>
+          <div>
+            欢迎来到XXXX！
+            <Button>登录</Button> 或 <Button>注册</Button>
+          </div>
+          <ul>
+            <li>
+              <Dropdown overlay={menu}>
+                <a href="#">
+                  Hover me <Icon type="down" />
+                </a>
+              </Dropdown>
+            </li>
+          </ul>
+        </Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
     );
   }
 };
