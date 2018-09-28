4e019c80def6a9053be36c13df516aa0623407e5
17600596130 <A932635@163.com>
2018-09-28 14:47:32 +0800

    fix( Pixijs )：react版本问题

--- src/components/pixiJs/index.js
+++ src/components/pixiJs/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Sprite, Stage } from "react-pixi-fiber";
-import PIXI from "pixi.js";
+import React, { Component } from "react";
+import { Stage, Sprite } from "react-pixi-fiber";
+import * as PIXI from "pixi.js";
 
 const height = 450;
-const width = 600;
+const width = 800;
 const OPTIONS = {
-  backgroundColor: 0x1099bb,
+  backgroundColor: 0x1099bb
 };
 const bunny = "https://i.imgur.com/IaUrttj.png";
 const centerAnchor = new PIXI.Point(0.5, 0.5);
 
-function PixiJs() {
-
-  console.log('pixi ---- ', PIXI);
-
-  return (
-    <Stage options={OPTIONS} width={width} height={height}>
-      <Sprite
-        anchor={centerAnchor}
-        texture={PIXI.Texture.fromImage(bunny)}
-        x={200} y={200}
-      />
-    </Stage>
-  )
+class PixiJs extends Component {
+  render() {
+    return (
+      <Stage options={OPTIONS} width={width} height={height}>
+        <Sprite
+          anchor={centerAnchor}
+          texture={PIXI.Texture.fromImage(bunny)}
+          x={400}
+          y={225}
+        />
+      </Stage>
+    );
+  }
 }
 
 export default PixiJs;
