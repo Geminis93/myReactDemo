8e25e934e27cae10e54cfa6fdc16e61896f1a20b
17600596130 <A932635@163.com>
2018-09-29 18:51:24 +0800

    feat( pixijs )：添加APP1，增加sprite

--- src/components/createJs/index.js
+++ src/components/createJs/index.js
@@ -3,7 +3,64 @@ import TweenOne from 'rc-tween-one';
 import './index.scss';
 
 class CreateJs extends Component {
+
+  change() {
+    const x = 100;
+    const beer = 5;
+    const pBeer = 3;
+    const gBeer = 5;
+
+    const a = x / beer;
+    const pa = parseInt(a / pBeer, 10);
+    const pas = a % pBeer;
+    const ga = a / gBeer;
+    const gas = a % gBeer;
+
+    const b = pa + ga;
+    const pb = (b + pas) / pBeer;
+    const pbs = (b + pas) % pBeer;
+    const gb = (b + gas) / gBeer;
+    const gbs = (b + gas) % gBeer;
+
+    const c = pb + gb;
+    const pc = (c + pbs) / pBeer;
+    const pcs = (c + pbs) % pBeer;
+    const gc = parseInt((c + gbs) / gBeer, 10);
+    const gcs = (c + gbs) % gBeer;
+
+    const d = pc + gc;
+    const pd = (d + pcs) / pBeer;
+    const pds = (d + pcs) % pBeer;
+    const gd = parseInt((d + gcs) / gBeer, 10);
+    const gds = (d + gcs) % gBeer;
+
+    const e = pd + gd;
+    const pe = parseInt((e + pds) / pBeer, 10);
+    const pes = parseInt((e + pds) % pBeer, 10);
+    const ge = parseInt((e + gds) / gBeer, 10);
+    const ges = parseInt((e + gds) % gBeer, 10);
+
+    const f = pe + ge;
+    const pf = parseInt((f + pes) / pBeer, 10);
+    const pfs = parseInt((f + pes) % pBeer, 10);
+    const gf = parseInt((f + ges) / gBeer, 10);
+    const gfs = parseInt((f + ges) % gBeer, 10);
+
+    console.log('a --- ', a);
+    
+    console.log('b --- ', b);
+    
+    console.log('c --- ', c);
+    
+    console.log('d --- ', d);
+    
+    console.log('e --- ', e);
+
+    console.log('f --- ', f);
+  }
+
   render() {
+    this.change();
     const itemList = [
       {
         left: -75,
--- src/components/pixiJs/index.js
+++ src/components/pixiJs/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { Stage, Sprite, render } from "react-pixi-fiber";
+import { Stage, Sprite, render, Text } from "react-pixi-fiber";
 import * as PIXI from "pixi.js";
 
 const height = 450;
 const width = 800;
 const OPTIONS = {
   backgroundColor: 0x1099bb,
-  view: document.getElementById('dom')
+  // view: document.getElementById('dom')
 };
 const bunny = "https://i.imgur.com/IaUrttj.png";
 const centerAnchor = new PIXI.Point(0.5, 0.5);
@@ -14,34 +14,66 @@ const centerAnchor = new PIXI.Point(0.5, 0.5);
 class PixiJs extends Component {
   state = {
     app: new PIXI.Application(width, height, OPTIONS),
+    app1: new PIXI.Application(width, height, OPTIONS),
     rotation: 0,
     x: 100,
   }
 
   componentDidMount() {
-    const { app } = this.state;
+    const { app, app1 } = this.state;
     app.ticker.add(this.animate);
     this.dom.appendChild(app.view);
+    this.dom.appendChild(app1.view);
+    this.setApp1();
   }
 
   animate = delta => {
+    const { app } = this.state;
     this.setState(state => ({
       ...state,
       rotation: state.rotation + 0.1 * delta,
-      x: state.x > 700 ? 100 : state.x + 1 * delta,
+      x: state.x > app.screen.width ? 0 : state.x + 5 * delta,
     }));
+  }
+
+  setApp1() {
+    const { app1 } = this.state;
+    const container = new PIXI.Container();
+    container.position.set(app1.screen.width / 2, app1.screen.height);
+    const surface = new PIXI.Sprite(new PIXI.Texture.fromImage("https://pixijs.io/examples/required/assets/bkg.jpg"));
+    surface.anchor.set(0.5, 1);
+    surface.width = app1.screen.width;
+    surface.height = app1.screen.height;
+
+    app1.stage.addChild(container);
+    container.addChild(surface);
+    console.log('container ----- ', container.proj)
   }
 
   render() {
     const { app, rotation, x } = this.state;
     /* 2 */
-    render(<Sprite
-      anchor={centerAnchor}
-      texture={PIXI.Texture.fromImage(bunny)}
-      rotation={rotation}
-      x={x}
-      y={225}
-    />, app.stage);
+    render(
+      [
+        <Text key="text" text="Hello World!" x={200} y={200} />,
+        <Sprite
+          key="Sprite"
+          anchor={centerAnchor}
+          texture={PIXI.Texture.fromImage(bunny)}
+          rotation={rotation}
+          x={x}
+          y={225}
+        />,
+        <Sprite
+          key="Sprite1"
+          anchor={centerAnchor}
+          texture={PIXI.Texture.fromImage(bunny)}
+          rotation={rotation}
+          x={x}
+          y={200}
+        />,
+      ]
+    , app.stage);
     return (
       <div ref={refs => this.dom = refs}>
         {/* 1 */}
