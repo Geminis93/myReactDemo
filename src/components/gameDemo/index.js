c1d772677eb53dd10e5cfbd7a96768c42035197d
17600596130 <A932635@163.com>
2018-10-12 18:43:20 +0800

    feat( gameDemo )：创建图形模块

--- src/components/gameDemo/index.js
+++ src/components/gameDemo/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import * as PIXI from "pixi.js";
+import bkg from '../../assets/images/bkg.jpg';
+import bunny from '../../assets/images/bunny.png';
 
 class GameDemo extends Component {
   state = {
@@ -25,14 +27,17 @@ class GameDemo extends Component {
   // 创建图形
   createGraphics(info) {
     const {
-      type,
-      x,
-      y,
-      width,
-      height,
-      color,
-      alpha,
-      borderRadius,
+      name,
+      type = 'rect',
+      x = 0,
+      y = 0,
+      width = 100,
+      height = 100,
+      color = 0x000000,
+      alpha = 1,
+      borderRadius = 0,
+      interactive = false,
+      buttonMode = false,
     } = info;
     const drawTypeMap = {
       roundedRect: 'drawRoundedRect',
@@ -40,12 +45,28 @@ class GameDemo extends Component {
     };
     const drawType = drawTypeMap[type];
     const graphics = new PIXI.Graphics();
+    graphics.name = name;
     graphics.beginFill(color, alpha);
     graphics[drawType](x, y, width, height, borderRadius);
     graphics.x = x;
     graphics.y = y;
+    graphics.interactive = interactive;
+    graphics.buttonMode = buttonMode;
     graphics.endFill();
     return graphics;
+  }
+
+  // 缓存图片
+  loaderImage(option) {
+    const loader = PIXI.loader.add(option);
+    return loader;
+  }
+
+  // 创建精灵
+  createSprite(option) {
+    const load = PIXI.loader.resources[option].texture;
+    const sprite = new PIXI.Sprite(load);
+    return sprite;
   }
 
   componentDidMount() {
@@ -54,6 +75,81 @@ class GameDemo extends Component {
       view,
     });
     this.dom.append(view.view);
+    setTimeout(() => {
+      this.drawView();
+    }, 0);
+  }
+
+  // 绘制 view
+  drawView() {
+    const { view } = this.state;
+    this.loaderImage([
+      {
+        name: 'bkg',
+        url: bkg,
+      },
+      {
+        name: 'bunny',
+        url: bunny,
+      }
+    ]).load(() => {
+      /* const bg = this.createSprite('bkg');
+      bg.alpha = 0.1;
+      view.stage.addChild(bg);
+      setTimeout(() => {
+        bg.alpha = 0.5;
+      }, 1000); */
+
+      // 模拟赌桌
+      const desk = this.createGraphics({
+        name: 'desk',
+        type: 'roundedRect',
+        x: 50,
+        y: 100,
+        width: 800,
+        height: 400,
+        borderRadius: 200,
+        color: 0xFF9933,
+      });
+      const desk1 = this.createGraphics({
+        name: 'desk1',
+        type: 'roundedRect',
+        x: 50,
+        y: 100,
+        width: 800,
+        height: 400,
+        borderRadius: 200,
+        alpha: 0.1
+      });
+      const left = this.createGraphics({
+        x: 100,
+        y: 125,
+        width: 250,
+        height: 300,
+        color: 0xFFFFFF,
+        alpha: 0.1,
+        interactive: true,
+      });
+      left.on('click', () => {
+        console.log('1');
+      });
+      const right = this.createGraphics({
+        x: 275,
+        y: 125,
+        width: 250,
+        height: 300,
+        color: 0xFFFFFF,
+        alpha: 0.1,
+        interactive: true,
+      });
+      right.on('click', () => {
+        console.log('2');
+      });
+      view.stage.addChild(desk);
+      view.stage.addChild(desk1);
+      view.stage.addChild(left);
+      view.stage.addChild(right);
+    });
   }
 
   render() {
