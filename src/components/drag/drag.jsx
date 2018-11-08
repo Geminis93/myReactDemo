1c0d29e335753cd11e72fb72352a5571dab79634
17600596130 <A932635@163.com>
2018-11-08 17:20:29 +0800

    feat( 购物车demo )：单个添加

--- src/components/drag/drag.jsx
+++ src/components/drag/drag.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Motion, spring } from 'react-motion';
 import './index.scss';
 
-const goods = new Array(10).fill({}).map((n, i) => {
+const goodsList = new Array(10).fill({}).map((n, i) => {
   return {
     id: i,
     info: `Goods ${i + 1}`,
@@ -36,10 +36,12 @@ class DemoDragList extends Component {
           text: 'text 4444',
         },
       ],
+      copyGoods: [],
     }
   }
   
   getCarInfo() {
+    const { copyGoods } = this.state;
     // 购物车位置
     const CX = this.cartDomA.offsetLeft;    
     const CY = this.cartDomA.offsetTop;
@@ -49,12 +51,31 @@ class DemoDragList extends Component {
     const goods = this.goodsDom.children[index];
     const GX = goods.offsetLeft;
     const GY = goods.offsetTop;
-    const good = [GX, GY];
     // 复制商品
+    const copylist = goodsList.filter((n, i) => i === index).map((item) => {
+      return {
+        ...item,
+        style: {
+          top: GY,
+          left: GX,
+          width: 150,
+          height: 30,
+          borderRadius: 0,
+          endTop: CY,
+          endLeft: CX,
+          opacity: 1,
+        },
+      };
+    });
+    this.setState({
+      copyGoods: copyGoods.concat(copylist),
+    });
     // 添加到动画列表
   }
 
   render() {
+    const { copyGoods } = this.state;
+    console.log(copyGoods);
     return (
       <div>
         <h3>Drag Demo</h3>
@@ -77,16 +98,52 @@ class DemoDragList extends Component {
           <div className="cart">
             <ul ref={refs => (this.goodsDom = refs)} className="goods-list">
               {
-                goods.map((item, i) => {
+                goodsList.map((item, i) => {
                   return <li key={item.id} className="goods">
                     { item.info }
                   </li>;
                 })
               }
             </ul>
-            <div className="car" ref={refs => (this.cartDomA = refs)}>A</div>
-            {/* <div className="car" ref={refs => (this.cartDomB = refs)}>B</div> */}
-            {/* <div className="car" ref={refs => (this.cartDomC = refs)}>C</div> */}
+            {
+              copyGoods.length > 0 && copyGoods.map((item, i) => {
+                const style = item.style;
+                return <Motion
+                  key={i}
+                  defaultStyle={style}
+                  style={{
+                    ...style,
+                    width: spring(30),
+                    height: spring(30),
+                    borderRadius: spring(30),
+                    top: spring(style.endTop),
+                    left: spring(style.endLeft),
+                    opacity: spring(0),
+                  }}
+                  onRest={() => {
+                    this[`motion${i}`].remove();
+                  }}>
+                  {interpolatingStyle =>
+                    <div style={{
+                      ...interpolatingStyle,
+                      width: `${interpolatingStyle.width}px`,
+                      height: `${interpolatingStyle.height}px`,
+                      borderRadius: interpolatingStyle.borderRadius,
+                      backgroundColor: '#F00',
+                      position: "absolute",
+                      top: interpolatingStyle.top,
+                      left: interpolatingStyle.left,
+                      opacity: interpolatingStyle.opacity,
+                  }}
+                  ref={refs => this[`motion${i}`] = refs}>{ item.info }</div>
+                  }
+                </Motion>})
+            }
+            <div className="cars">
+              <div className="car" ref={refs => (this.cartDomA = refs)}>A</div>
+              <div className="car" ref={refs => (this.cartDomB = refs)}>B</div>
+              <div className="car" ref={refs => (this.cartDomC = refs)}>C</div>
+            </div>
           </div>
         </div>
       </div>
