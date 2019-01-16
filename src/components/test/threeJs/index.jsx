560b002d1f256f81f2f7f96555ae6e53c817324a
17600596130 <A932635@163.com>
2019-01-16 19:03:52 +0800

    feat( threeJs )：TODO

---
+++ src/components/test/threeJs/index.jsx
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class ThreeJsDemo extends PureComponent {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+ThreeJsDemo.propTypes = {
+
+};
+
+export default ThreeJsDemo;
